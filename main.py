dc9fda29fe79b07dbcc16a3c3ca75b583d94a7cb
Mrityunjay Shukla <107771951+mrityunjayatgh@users.noreply.github.com>
2024-05-09 04:32:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
+def print_hi(name): 
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
@@ -13,4 +13,3 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
